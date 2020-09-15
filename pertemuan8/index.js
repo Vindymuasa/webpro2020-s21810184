4b6f3f0be95ec0bee39fadd78e6119a0dac054ec
Vindymuasa <s21810184@student.unklab.ac.id>
2020-09-15 22:50:49 +0800

    menambahkan pertemuan8

---
+++ pertemuan8/index.js
@@ -0,0 +1,25 @@
+let account = {
+    name: "Vin",
+    expenses: [],
+    addExpenses: function(description, amount){
+        this.expenses.push({"description": description, "amount": amount});
+    },
+
+    getAccountSummary: function (){
+        let totalExpenses = 0;
+        this.expenses.forEach(function(el){
+            totalExpenses = totalExpenses + el.amount;
+        });
+    return totalExpenses;
+    }
+};
+
+account.addExpenses('Bayar Tagihan Listrik', 250000);
+account.addExpenses('Beli Twister', 18000);
+account.addExpenses('Beli Wafer Selamat', 23000)
+console.log("Total pengeluaran " + account.name +" adalah Rp." + account.getAccountSummary());
+
+
+
+
+
