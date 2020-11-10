d19e879961da5778f36daf173856d305bbb5d966
Vindymuasa <S21810184@student.com-->
2020-11-10 11:53:07 +0800

    menambahkan pertemuan20

---
+++ pertemuan20/src/Components/Contents.js
@@ -0,0 +1,11 @@
+impor  bereaksi  dari  'bereaksi' ;
+
+ Konten  const =  ( )  => {
+    kembali  (
+      < p >
+        Lorem Ipsum hanyalah teks tiruan dari industri percetakan dan penyusunan huruf. Lorem Ipsum telah menjadi teks tiruan standar industri sejak tahun 1500-an, ketika printer yang tidak dikenal menggunakan galley jenis dan mengacaknya untuk membuat buku spesimen jenis. Ini telah bertahan tidak hanya lima abad, tetapi juga lompatan ke dalam penyusunan huruf elektronik, tetap pada dasarnya tidak berubah. Itu dipopulerkan pada 1960-an dengan rilis lembaran Letraset yang berisi bagian-bagian Lorem Ipsum, dan baru-baru ini dengan perangkat lunak penerbitan desktop seperti Aldus PageMaker termasuk versi Lorem Ipsum
+      < / p >
+    ) ;
+  } ;
+
+ekspor Konten default;
---
+++ pertemuan20/src/Components/Header.js
@@ -0,0 +1,12 @@
+ import React form 'react';
+
+ const Header = () =>{
+     return (
+         < >
+             <h1> Basic React </h1>
+             <h2> Component React </h2>
+         </>
+     );
+ };
+
+ ekspor default Header;
