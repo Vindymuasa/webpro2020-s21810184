8e3932bd40b6a01dabad411b05bfbd0c828c3dbd
Vindymuasa <S21810184@student.com-->
2020-11-13 00:46:45 +0800

    menambahkan pertemuan16

---
+++ pertemuan16/hello.js
@@ -0,0 +1,10 @@
+// export const numbers = [1,2,3,4,5];
+
+// export const world = (user) => {
+//    console.log ('Hello World');
+// }
+
+const hello = (user) => {
+    console.log ('Hello, ${user}');
+}
+export default hello;
---
+++ pertemuan16/index.js
@@ -0,0 +1,10 @@
+// import { hello, world, numbers } from './hello.js'
+
+// import * as helper from './hello.js'
+
+import Func1 from './hello.js';
+Func1 ('Joe Doe');
+
+// helper.hello('Jhon Doe');
+// helper.world();
+// console.log(helper.numbers);
